e92bbb7246341f0479e4e05f44a3b7beabbb4ad7
Maximilian Fischer <github@maaeps.de>
2024-01-03 23:44:16 +0100

    convert_color_temp

--- esphome/components/rgbct_virtual/light.py
+++ esphome/components/rgbct_virtual/light.py
@@ -9,10 +9,25 @@ from esphome.const import (
     CONF_LAMBDA,
 )
 
+CONF_CONVERT_COLOR_TEMP = "convert_color_temp"
+
 CODEOWNERS = ["@fischerscode"]
 
 rgbct_virtual_ns = cg.esphome_ns.namespace("rgbct_virtual")
 VirtualRGBCTLightOutput = rgbct_virtual_ns.class_("VirtualRGBCTLightOutput", light.LightOutput)
+
+def validate_conversion_requires_interlock(value):
+    if (
+        CONF_COLOR_INTERLOCK in value
+        and CONF_CONVERT_COLOR_TEMP in value
+        and value[CONF_CONVERT_COLOR_TEMP]
+        and not [CONF_COLOR_INTERLOCK]
+    ):
+        raise cv.Invalid(
+            "Color temperature conversion requires enabling color interlock.",
+            path=[CONF_CONVERT_COLOR_TEMP],
+        )
+    return value
 
 CONFIG_SCHEMA = cv.All(
     light.RGB_LIGHT_SCHEMA.extend(
@@ -22,11 +37,12 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_WARM_WHITE_COLOR_TEMPERATURE): cv.color_temperature,
             cv.Optional(CONF_COLOR_INTERLOCK, default=False): cv.boolean,
             cv.Optional(CONF_LAMBDA): cv.lambda_,
+            cv.Optional(CONF_CONVERT_COLOR_TEMP, default=False): cv.boolean, 
         }
     ),
     light.validate_color_temperature_channels,
+    validate_conversion_requires_interlock,
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
@@ -40,3 +56,4 @@ async def to_code(config):
     cg.add(var.set_warm_white_temperature(config[CONF_WARM_WHITE_COLOR_TEMPERATURE]))
 
     cg.add(var.set_color_interlock(config[CONF_COLOR_INTERLOCK]))
+    cg.add(var.set_convert_color_temp(config[CONF_CONVERT_COLOR_TEMP]))
--- esphome/components/rgbct_virtual/rgbct_light_output.h
+++ esphome/components/rgbct_virtual/rgbct_light_output.h
@@ -17,6 +17,7 @@ class VirtualRGBCTLightOutput : public light::LightOutput {
   void set_cold_white_temperature(float cold_white_temperature) { cold_white_temperature_ = cold_white_temperature; }
   void set_warm_white_temperature(float warm_white_temperature) { warm_white_temperature_ = warm_white_temperature; }
   void set_color_interlock(bool color_interlock) { color_interlock_ = color_interlock; }
+  void set_convert_color_temp(bool convert_color_temp) { convert_color_temp_ = convert_color_temp; }
 
   light::LightTraits get_traits() override {
     auto traits = light::LightTraits();
@@ -32,6 +33,48 @@ class VirtualRGBCTLightOutput : public light::LightOutput {
     float red, green, blue, color_temperature, white_brightness;
 
     state->current_values_as_rgbct(&red, &green, &blue, &color_temperature, &white_brightness);
+
+    if (white_brightness > 0 && this->convert_color_temp_) {
+      float temp = 10000 / color_temperature;
+
+      if (temp <= 66) {
+        red = 1;
+      } else {
+        red = 1.292936186062745 * pow(temp - 60, -0.1332047592);
+        if (red < 0) {
+          red = 0;
+        }
+        if (red > 1) {
+          red = 1;
+        }
+      }
+
+      if (temp <= 66) {
+        green = 0.3900815787690196 * log(temp) - 0.6318414437886275;
+      } else {
+        green = 1.129890860895294 * pow(temp - 60, -0.0755148492);
+      }
+      if (green < 0) {
+        green = 0;
+      }
+      if (green > 1) {
+        green = 1;
+      }
+
+      if (temp >= 66) {
+        blue = 1;
+      } else if (temp <= 19) {
+        blue = 0;
+      } else {
+        blue = 0.543206789110196 * log(temp - 10) - 1.19625408914;
+        if (blue < 0) {
+          blue = 0;
+        }
+        if (blue > 1) {
+          blue = 1;
+        }
+      }
+    }
 
     if (this->func_ != nullptr) {
       this->func_(red, green, blue, color_temperature, white_brightness);
@@ -49,6 +92,7 @@ class VirtualRGBCTLightOutput : public light::LightOutput {
   float cold_white_temperature_;
   float warm_white_temperature_;
   bool color_interlock_{true};
+  bool convert_color_temp_;
 };
 
 }  // namespace rgbct_virtual

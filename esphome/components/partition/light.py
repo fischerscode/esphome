0fb7c995f2594af3a9052e4a34c0f139130ee321
Maximilian Fischer <github@maaeps.de>
2024-01-04 01:01:20 +0100

    Revert "simplify and maybe fix light partition"
    This reverts commit 52cd6709965047a470b88ac97beff1f553a4f3ed.

--- esphome/components/partition/light.py
+++ esphome/components/partition/light.py
@@ -98,14 +98,13 @@ async def to_code(config):
     segments = []
     for conf in config[CONF_SEGMENTS]:
         if CONF_SINGLE_LIGHT_ID in conf:
-            segments.append(
-                AddressableSegment(
-                    await cg.get_variable(conf[CONF_SINGLE_LIGHT_ID]),
-                    0, 
-                    1, 
-                    False
-                )
+            wrapper = cg.new_Pvariable(
+                conf[CONF_ADDRESSABLE_LIGHT_ID],
+                await cg.get_variable(conf[CONF_SINGLE_LIGHT_ID]),
             )
+            light_state = cg.new_Pvariable(conf[CONF_LIGHT_ID], wrapper)
+            await cg.register_component(light_state, conf)
+            segments.append(AddressableSegment(light_state, 0, 1, False))
 
         else:
             segments.append(
